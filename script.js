6f915127e72a5d9d1c44624ca37ccce8a2d68f36
opedromeireles <pedrinhobelezaa@gmail.com>
2022-11-26 10:26:21 -0300

    Modificações: tela login

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-botao = document.querySelector("#botao-jogar");
+let nomeinput = document.querySelector("#nome").value; 
+let senhainput = document.querySelector("#senha").value; 
+let botao = document.querySelector("#botao-jogar");
+
+let criarconta = document.querySelector("#botao-login");
+criarconta.addEventListener("click", function(){
+    
+}); 
+
+
+
+
+
+
 botao.addEventListener("click", function(){
     alert("on");
     let nomeinput = document.querySelector("#nome").value; 
-let idadeinput = document.querySelector("#idade").value; 
+    let senhainput = document.querySelector("#senha").value; 
 
 window.localStorage.setItem("Nome", nomeinput);
-window.localStorage.setItem("Idade", idadeinput);
+window.localStorage.setItem("senha", senhainput);
 });
+

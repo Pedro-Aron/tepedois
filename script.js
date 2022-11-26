a848193a17ad6783758a2440b014487fa66489ba
d4ruh <rudabrandao@ruoso.com>
2022-11-26 11:12:57 -0300

    login rustico mas funcional implementado

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
-let nomeinput = document.querySelector("#nome").value; 
-let senhainput = document.querySelector("#senha").value; 
 let botao = document.querySelector("#botao-jogar");
+
+let contas = [];
 
 let criarconta = document.querySelector("#botao-login");
 criarconta.addEventListener("click", function(){
-    
+    let nomeinput = document.querySelector("#nome").value; 
+    let senhainput = document.querySelector("#senha").value; 
+    if(nomeinput.length < 1 || senhainput.length < 8) {
+        alert("nome/senha inválidos");
+        return;
+    };
+
+    for (let item of contas) {
+        if(nomeinput == item.nome) {
+            return;
+        }
+    }
+
+    contas.push({nome: nomeinput, senha: senhainput});
+
+    console.log(contas);
+
 }); 
 
 botao.addEventListener("click", function(){
     alert("on");
-    let nomeinput = document.querySelector("#nome").value; 
-    let senhainput = document.querySelector("#senha").value; 
-
-window.localStorage.setItem("Nome", nomeinput);
-window.localStorage.setItem("senha", senhainput);
+    window.localStorage.setItem("Nome", nomeinput);
+    window.localStorage.setItem("senha", senhainput);
 });
-

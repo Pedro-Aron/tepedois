5e306af5e20e7bfede401a477f510e9d67be557c
d4ruh <rudabrandao@ruoso.com>
2022-11-26 11:28:56 -0300

    loginzin melhor do melhor

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let botao = document.querySelector("#botao-jogar");
+let criarconta = document.querySelector("#botao-login");
 
 let contas = [];
 
-let criarconta = document.querySelector("#botao-login");
 criarconta.addEventListener("click", function(){
     let nomeinput = document.querySelector("#nome").value; 
     let senhainput = document.querySelector("#senha").value; 
@@ -18,13 +18,27 @@ criarconta.addEventListener("click", function(){
     }
 
     contas.push({nome: nomeinput, senha: senhainput});
-
-    console.log(contas);
-
 }); 
 
 botao.addEventListener("click", function(){
-    alert("on");
-    window.localStorage.setItem("Nome", nomeinput);
-    window.localStorage.setItem("senha", senhainput);
+    let nomeinput = document.querySelector("#nome").value; 
+    let senhainput = document.querySelector("#senha").value; 
+    let count = 0;
+
+    for (let item of contas) {
+        if(nomeinput == item.nome) {
+            count ++;
+            if (senhainput == item.senha) {
+                //entrar na pagina do jogo
+            }
+            else {
+                alert("senha incorreta");
+                return;
+            }
+        }
+    }
+
+    if (count == 0) {
+        alert("conta não registrada");
+    }
 });

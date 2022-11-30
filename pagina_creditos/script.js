381525301b376de1de5e401e187878bf7a0721a0
ruda <rudabrandao@ruoso.com>
2022-11-30 14:52:24 -0300

    ranking dos melhores implementado

--- pagina_creditos/script.js
+++ pagina_creditos/script.js
@@ -1,5 +1,5 @@
 botao = document.querySelector("#retornar");
 
 botao.addEventListener("click", function(){
-    location.href = "../pagina_login/index.html";
+    location.href = "../index.html";
 });
--- pagina_jogo/script.js
+++ pagina_jogo/script.js
@@ -3,7 +3,12 @@ let textoVitoriaEl = document.querySelector("#texto-vitoria");
 let btnVitoriaEL = document.querySelector("#btn-vitoria");
 let botao = document.querySelector("#botao-jogar");
 let criarconta = document.querySelector("#botao-criar");
-let ranks = document.querySelector("#botao-ranking");
+let linkCredLog = document.querySelector("#botao-creditos");
+let linkCredVit = document.querySelector("#btn-creditos");
+let voltarLog = document.querySelector("#btn-retorno");
+let btnRanking = document.querySelector("#botao-ranking");
+let crpTabela = document.querySelector("#corpo-tabela");
+let btnVoltarRank = document.querySelector("#btn-rank-voltar");
 
 let count = 0;
 
@@ -22,16 +27,18 @@ let seqVitoria = [
 
 let contas = [{
     nome: "teste1",
-    senha: "teste1",
+    senha: "qogbvoiadb",
     nVitorias: 0, 
     nPartidas: 0
 },
 {
     nome: "teste2",
-    senha: "teste2",
+    senha: "apsifnapvdkp",
     nVitorias: 0, 
     nPartidas: 0
 }]
+
+contas = JSON.parse(localStorage.getItem("contas"));
 
 for (let item of posEl) {
     item.addEventListener("click", function() {
@@ -41,7 +48,7 @@ for (let item of posEl) {
 
             count++;
 
-            (count) % 2 ? checkVitoria("x") : checkVitoria("o");
+            (count % 2) ? checkVitoria("x") : checkVitoria("o");
          }
     });
 }
@@ -57,25 +64,48 @@ function checkVitoria(jogador) {
         count = 0;
         document.getElementById("vitoria").style.display = "flex";
         textoVitoriaEl.innerHTML = `${jogador.toUpperCase()} venceu!`;
+        let contaLogada = localStorage.getItem("conta-logada");
+        for (let item of contas) {
+            if (item.nome == contaLogada) {
+                item.nVitorias++;
+                item.nPartidas++;
+                qs(contas);
+                console.log(contas);
+                break;
+            }
+        }
+        localStorage.setItem("contas", JSON.stringify(contas));
     }
 
     if (count == 9 && !vitoria) {
         count = 0;
         document.getElementById("vitoria").style.display = "flex";
         textoVitoriaEl.innerHTML = `Empate!`;
+        let contaLogada = localStorage.getItem("conta-logada");
+        for (let item of contas) {
+            if (item.nome == contaLogada) {
+                item.nPartidas++;
+                break;
+            }
+        }
+        localStorage.setItem("contas", JSON.stringify(contas));
     }
 
     return;
 }
 
 btnVitoriaEL.addEventListener("click", function() {
+    clearBoard();
+});
+
+function clearBoard() {
+    document.getElementById("vitoria").style.display = "none";
+
     for (let item of posEl) {
         item.classList.remove("x");
         item.classList.remove("o");
     }
-
-    document.getElementById("vitoria").style.display = "none";
-});
+}
 
 criarconta.addEventListener("click", function(){
     let nomeinput = document.querySelector("#nome").value; 
@@ -94,8 +124,9 @@ criarconta.addEventListener("click", function(){
     }
 
     let contaNova = {nome: nomeinput, senha: senhainput, nVitorias: 0, nPartidas: 0};
-
     contas.push(contaNova);
+
+    localStorage.setItem("contas", JSON.stringify(contas));
 
     alert("Conta registrada com sucesso!");
 }); 
@@ -110,7 +141,10 @@ botao.addEventListener("click", function(){
         if(nomeinput == item.nome) {
             count++;
             if (senhainput == item.senha) {
-                document.getElementById("login-aparece").style.display = "none";                
+                document.querySelector("#nome").value = "";
+                document.querySelector("#senha").value = "";
+                localStorage.setItem("conta-logada", `${nomeinput}`);
+                document.getElementById("login-aparece").style.display = "none";
             }
             else {
                 alert("Senha Incorreta");
@@ -123,6 +157,73 @@ botao.addEventListener("click", function(){
     
 });
 
-ranks.addEventListener("click", function() {
-    location.href = "../pagina_creditos/creditos.html"
+linkCredLog.addEventListener("click", function() {
+    location.href = "pagina_creditos/creditos.html"
 });
+
+linkCredVit.addEventListener("click", function() {
+    location.href = "pagina_creditos/creditos.html"
+});
+
+voltarLog.addEventListener("click", function() {
+    document.getElementById("login-aparece").style.display = "flex";
+    localStorage.setItem("conta-logada", `none`);
+    document.getElementById("vitoria").style.display = "none";
+    clearBoard();
+});
+
+
+
+
+function qs(vet)
+{
+    quickSort(vet, 0, vet.length - 1);
+}
+
+function quickSort(vet, ini, fim)
+{
+    var i = ini;
+    var f = fim;
+    var m = Math.floor((i + f)/2);
+
+    while(i < f)
+    {
+        while(vet[i].nVitorias > vet[m].nVitorias)
+            i++;
+
+        while(vet[f].nVitorias < vet[m].nVitorias)
+            f--;
+
+        if(i <= f)
+        {
+            var temp = vet[i];
+            vet[i] = vet[f];
+            vet[f] = temp;
+            i++;
+            f--;
+        }
+    }
+
+    if(f > ini)
+        quickSort(vet, ini, f);
+
+    if(i < fim)
+        quickSort(vet, i, fim);
+}
+
+btnRanking.addEventListener("click", function() {
+    document.getElementById("ranking").style.display = "flex";
+
+    crpTabela.innerHTML = "";
+
+    for (let i=0; i<10; i++) {
+        crpTabela.innerHTML += `<tr><td>${i+1}°</td><td>${contas[i].nome}</td><td>${contas[i].nVitorias}</td><td>${contas[i].nPartidas}</td></tr>`
+    }
+
+    document.getElementById("login-aparece").style.display = "none";
+})
+
+btnVoltarRank.addEventListener("click", function() {
+    document.getElementById("ranking").style.display = "none";
+    document.getElementById("login-aparece").style.display = "flex";
+})
